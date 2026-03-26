82e0a81adf7a1aa93d7556095123574cd1d64e2a
Imrefox <karl.suhajda@yahoo.com>
2026-03-26 11:13:11 +0100

    first commit

---
+++ karlswift/karlswift/karlswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  karlswiftApp.swift
+//  karlswift
+//
+//  Created by Karl Suhajda on 2026. 03. 26..
+//
+
+import SwiftUI
+
+@main
+struct karlswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
